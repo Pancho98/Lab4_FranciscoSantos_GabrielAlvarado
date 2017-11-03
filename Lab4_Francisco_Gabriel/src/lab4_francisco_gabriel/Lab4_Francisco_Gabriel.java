4d5bb27efe871a5de5f25957bd1d337082c25263
Gabriel Alvarado <gabriel_alvararado24@hotmail.com>
2017-11-03 14:15:33 -0600

    Eliminar

--- Lab4_Francisco_Gabriel/src/lab4_francisco_gabriel/Lab4_Francisco_Gabriel.java
+++ Lab4_Francisco_Gabriel/src/lab4_francisco_gabriel/Lab4_Francisco_Gabriel.java
@@ -8,71 +8,64 @@ import javax.swing.JOptionPane;
  *
  * @author Francisco Santos
  */
+
+
 public class Lab4_Francisco_Gabriel {
 
-    static ArrayList<Magos> ListMagos= new ArrayList();
-    static ArrayList<Elgos> ListElfos= new ArrayList();
-    static ArrayList<Dragones> ListDragones= new ArrayList();
-    static ArrayList<Arqueros> ListArqueros= new ArrayList();
-    static ArrayList<Brujas> ListBrujas= new ArrayList();
-    
-    public static void main(String[] args) {
-    String opcion="";
-    while(!opcion.equalsIgnoreCase("g")){
-    opcion = JOptionPane.showInputDialog("MENU\n"
-            + "a.Creacion de Guerreros\n"
-            + "b.Eliminacion de Guerrero\n"
-            + "c.Registro de Jugardores\n"
-            + "d.Iniciar Partidad\n"
-            + "e.Listar Jugadores\n"
-            + "f.Cargar partidad"
-            + "g.Salir\n");
-        if (opcion.equalsIgnoreCase("a")) {
-            String jugadores="";
-            jugadores = JOptionPane.showInputDialog("MENU\n"
-                    + "a.Crear Magos\n"
-                    + "b.Crear Elgos\n"
-                    + "c.Crear Dragones\n"
-                    + "d.Crear Arqueros\n"
-                    + "e.Crear Brujas\n");
-            if (jugadores.equalsIgnoreCase("a")) {
-                String tipo = JOptionPane.showInputDialog("Ingrese tipo de magia");
-                String elemento = JOptionPane.showInputDialog("Ingrese elemento favorito");
-            }
-            if (jugadores.equalsIgnoreCase("b")) {
-                String tipo = JOptionPane.showInputDialog("Ingrese tipo de arma");
-                String rango = JOptionPane.showInputDialog("Ingrese Rango");
-            }
-            if (jugadores.equalsIgnoreCase("c")) {
-                String color = JOptionPane.showInputDialog("Ingrese color");
-                String raza = JOptionPane.showInputDialog("Ingres raza");
-            }
-            if (jugadores.equalsIgnoreCase("d")) {
-                String materialA =JOptionPane.showInputDialog("Ingrese material de Arco");
-                String materiaAr = JOptionPane.showInputDialog("Ingrese material de armadura");
-            }
-            if (jugadores.equalsIgnoreCase("e")) {
-                String siglo = JOptionPane.showInputDialog("Ingrese siglo de nacimiento");
-                String lugar = JOptionPane.showInputDialog("Ingrese lugar de residencia");
-            }
-            
-        }
-        if (opcion.equalsIgnoreCase("b")) {
-            
-        }
-        if (opcion.equalsIgnoreCase("c")) {
-            
-        }
-        
-            
-=======
-public class Lab4_Francisco_Gabriel {
+    static ArrayList<Magos> ListMagos = new ArrayList();
+    static ArrayList<Elfos> ListElfos = new ArrayList();
+    static ArrayList<Dragones> ListDragones = new ArrayList();
+    static ArrayList<Arqueros> ListArqueros = new ArrayList();
+    static ArrayList<Brujas> ListBrujas = new ArrayList();
 
-    
     public static void main(String[] args) {
-        
->>>>>>> 2651ba23587113ad739fc8409997bd90385bb3a2
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("g")) {
+            opcion = JOptionPane.showInputDialog("MENU\n"
+                    + "a.Creacion de Guerreros\n"
+                    + "b.Eliminacion de Guerrero\n"
+                    + "c.Registro de Jugardores\n"
+                    + "d.Iniciar Partidad\n"
+                    + "e.Listar Jugadores\n"
+                    + "f.Cargar partidad"
+                    + "g.Salir\n");
+            if (opcion.equalsIgnoreCase("a")) {
+                String jugadores = "";
+                jugadores = JOptionPane.showInputDialog("MENU\n"
+                        + "a.Crear Magos\n"
+                        + "b.Crear Elgos\n"
+                        + "c.Crear Dragones\n"
+                        + "d.Crear Arqueros\n"
+                        + "e.Crear Brujas\n");
+                if (jugadores.equalsIgnoreCase("a")) {
+                    String tipo = JOptionPane.showInputDialog("Ingrese tipo de magia");
+                    String elemento = JOptionPane.showInputDialog("Ingrese elemento favorito");
+                }
+                if (jugadores.equalsIgnoreCase("b")) {
+                    String tipo = JOptionPane.showInputDialog("Ingrese tipo de arma");
+                    String rango = JOptionPane.showInputDialog("Ingrese Rango");
+                }
+                if (jugadores.equalsIgnoreCase("c")) {
+                    String color = JOptionPane.showInputDialog("Ingrese color");
+                    String raza = JOptionPane.showInputDialog("Ingres raza");
+                }
+                if (jugadores.equalsIgnoreCase("d")) {
+                    String materialA = JOptionPane.showInputDialog("Ingrese material de Arco");
+                    String materiaAr = JOptionPane.showInputDialog("Ingrese material de armadura");
+                }
+                if (jugadores.equalsIgnoreCase("e")) {
+                    String siglo = JOptionPane.showInputDialog("Ingrese siglo de nacimiento");
+                    String lugar = JOptionPane.showInputDialog("Ingrese lugar de residencia");
+                }
+
+            }
+            if (opcion.equalsIgnoreCase("b")) {
+
+            }
+            if (opcion.equalsIgnoreCase("c")) {
+
+            }
+
+        }
     }
-    
-}
 }
